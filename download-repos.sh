aff5faccd96b05dd7fd2e2d3130b5b134668edda
Zoran Olujic <olujicz@gmail.com>
2018-11-01 22:06:20 +0100

    Updated docker-compose file with latest changes.

--- download-repos.sh
+++ download-repos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PROJECT_ROOT=`dirname $0`
-mkdir "${PROJECT_ROOT}/projects"
+mkdir "${PROJECT_ROOT}/services"
 git clone https://github.com/one-love/backend "${PROJECT_ROOT}/services/backend"
 git clone https://github.com/one-love/frontend "${PROJECT_ROOT}/services/frontend"
